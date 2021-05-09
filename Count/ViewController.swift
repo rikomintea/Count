40d718ba9142e450017fcccf88b5fe3221514d1a
Riko Yasuda <rikomintea1827@gmail.com>
2021-05-09 18:01:35 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by スマート・ナビ on 2021/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
